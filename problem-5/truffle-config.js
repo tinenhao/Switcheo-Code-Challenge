ceeaa05402c83fa07937d1e7c25c5a51e76cd864
tinenhao <tinenhao@u.nus.edu>
2022-11-11 02:56:10 +0800

    deployed on sepolia testnet

--- problem-5/truffle-config.js
+++ problem-5/truffle-config.js
@@ -1,20 +1,31 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const sepoliaKey = "d3c18b9000d34246acaa24e1b5622bab";
+
+const fs = require("fs");
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
-    development: {
-     host: "127.0.0.1",
-     port: 8545,
-     network_id: "*"
+    sepolia: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://sepolia.infura.io/v3/${sepoliaKey}`
+        ),
+      network_id: 11155111,
+      gas: 5500000,
+      confirmation: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
     },
-    dashboard: {
-    }
   },
   compilers: {
     solc: {
       version: "0.8.13",
-    }
+    },
   },
   db: {
     enabled: false,
     host: "127.0.0.1",
-  }
+  },
 };
